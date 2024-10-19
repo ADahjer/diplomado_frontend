f665e9f52b71da1e9aafabe10647a235394b2130
mejia777 <mejiabsamuel777@gmail.com>
2024-10-19 10:33:06 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Ruteo } from "./app/utilidades/rutas/Ruteo";
 import { Cabecera } from "./app/componentes/contenedor/Cabecera";
 import { AuthProvider } from "./app/componentes/contenedor/Authcontext";
+import { AuthContext } from "./app/componentes/contenedor/Authcontext";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
